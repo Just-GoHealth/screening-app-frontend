411f7709210165066a58845c860f5dd36ce4e1a8
boypaida12 <baddysackey5@gmail.com>
2023-05-08 08:42:55 +0000

    added Landing Page and Health Records Pop Up

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 		<>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-				<Route path='/access-health-records' element={<AccessHealthRecords />}></Route>
+				<Route path="/access-health-records" element={<AccessHealthRecords />}></Route>
 				<Route path="/all-health-records" element={<AllHealthRecordsPage />} />
 				<Route
 					path="/school-health-records"
