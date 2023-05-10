8ee5722e5746517843776dd8ac595c07a49d0c07
boypaida12 <baddysackey5@gmail.com>
2023-05-10 22:37:15 +0000

    modified app.js route to addnewschool

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { ScreeningPage } from './views/screening';
 import { SchoolHealthRecordsPage } from './views/school-health-records';
 import { SchoolHealthSummaryPage } from './views/school-health-summary';
 import { AccessHealthRecords } from './views/access-health-records/AccessHealthRecords';
+import { AddNewSchool } from './views/add-new-school/AddNewSchool';
 
 const App = () => {
 	return (
@@ -17,7 +18,8 @@ const App = () => {
 				<Route
 					path="/access-health-records"
 					element={<AccessHealthRecords />}
-				></Route>
+				/>
+        <Route path="/add-new-school" element={<AddNewSchool/>} />
 				<Route path="/all-health-records" element={<AllHealthRecordsPage />} />
 				<Route
 					path="/school-health-records"
@@ -38,46 +40,6 @@ const App = () => {
 			</Routes>
 		</>
 	);
-
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./views/home/HomePage";
-import { SchoolHealthRecordsPage } from "./views/school-health-records/SchoolHealthRecordsPage";
-import { SchoolHealthSummaryPage } from "./views/school-health-summary/SchoolHealthSummaryPage";
-import { UserHealthSummaryPage } from "./views/user-health-summary";
-import { AllHealthRecordsPage } from "./views/all-health-records";
-import { PageNotFoundPage } from "./views/page-not-found";
-import { AccessHealthRecords } from "./views/access-health-records/AccessHealthRecords";
-import { AddNewSchool } from "./views/add-new-school/AddNewSchool";
-
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/access-health-records"
-          element={<AccessHealthRecords />}
-        />
-        <Route path="/add-new-school" element={<AddNewSchool />} />
-        <Route path="/all-health-records" element={<AllHealthRecordsPage />} />
-        <Route
-          path="/school-health-records"
-          element={<SchoolHealthRecordsPage />}
-        />
-        <Route
-          path="/school-health-summary"
-          element={<SchoolHealthSummaryPage />}
-        />
-        <Route
-          path="/user-health-summary"
-          element={<UserHealthSummaryPage />}
-        />
-
-        <Route path="*" element={<PageNotFoundPage />} />
-      </Routes>
-    </>
-  );
 };
 
 export default App;
