7fa34e8814e55e21122a344136b338cfa9b7717c
Philemon Forson <firstkoolaid21@gmail.com>
2023-05-14 17:31:23 +0000

    completed recommendations screen and additional edits

--- src/shared/components/navbar/Navbar.jsx
+++ src/shared/components/navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import { IconButton } from '@mui/material';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { logo } from '../../../assets/images';
 
-export const Navbar = ({ showBackButton, showLogo }) => {
+export const Navbar = ({ showBackButton, showLogo, showRecommendations }) => {
 	return (
-		<div className="nav-container">
-			<div>
+		<div className={showRecommendations ? "flex gap-x-10 h-[10vh] " :"nav-container"}>
+			<div className={showRecommendations ? 'basis-[20%] pl-20 ': ''}>
 				{showBackButton && (
 					<IconButton size="small" style={{ background: '#BCBEC0' }}>
 						<AiOutlineLeft color="white" />
@@ -15,9 +15,9 @@ export const Navbar = ({ showBackButton, showLogo }) => {
 				)}
 			</div>
 
-			<div>
+			<div className={showRecommendations ? 'flex-grow-2 basis-[80%] bg-[#DAE7F6] pr-20 py-[2.4vh]' : ''}>
 				{showLogo && (
-					<img src={logo} alt="Just Go Health Logo" className="h-8 sm:h-10" />
+					<img src={logo} alt="Just Go Health Logo" className="h-8 sm:h-10 float-right" />
 				)}
 			</div>
 		</div>
--- src/views/screening/ScreeningPage.jsx
+++ src/views/screening/ScreeningPage.jsx
@@ -1,37 +1,52 @@
-import { Button } from '@mui/material';
-import React, { useState } from 'react';
-import { Navbar } from '../../shared/components/navbar/Navbar';
-import './Screening.styles.css';
-import data from '../../shared/data/data.json';
-import MultiStepForm from './forms/MultiStepForm';
-import { useInAppNavigation } from '../../shared/custom-hooks/useInAppNavigation';
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { Navbar } from "../../shared/components/navbar/Navbar";
+import "./Screening.styles.css";
+import data from "../../shared/data/data.json";
+import MultiStepForm from "./forms/MultiStepForm";
+import { useInAppNavigation } from "../../shared/custom-hooks/useInAppNavigation";
+import GetRecommendations from "./recommendations/GetRecommendations";
 
 export const ScreeningPage = () => {
   const [selectedSection, setSelectedSection] = useState(1);
   const [selectedSubSection, setSelectedSubSection] = useState(1);
   const { viewHealthRecords } = useInAppNavigation();
   const [formData, setFormData] = useState({});
+  const [getRecommendations, setGetRecommendations] = useState(false);
 
+  //function to handle section change
   const handleSelectedSection = (id, subSectionId) => {
+    if(getRecommendations){ setGetRecommendations(false)}
     setSelectedSection(id);
     setSelectedSubSection(subSectionId);
   };
+
+    //function to handle subSection change
   const handleSelectedSubSection = (id) => {
     setSelectedSubSection(id);
   };
 
-  const startScreening = () => {
+  //function to handle recommendations page
+  const handleGetRecommendations = () => {
+    setGetRecommendations(true)
+    setSelectedSection(0),
+    setSelectedSubSection(0)
     setFormData({})
+  }
+
+  //function to start screening all over again
+  const startScreening = () => {
+    setFormData({});
     handleSelectedSection(data.data[0].id, data.data[0].subSections[0].id);
   };
   return (
     <>
       <>
-        <Navbar showLogo />
+        <Navbar showLogo showRecommendations={getRecommendations}/>
       </>
 
-      <div className=" flex mx-auto gap-x-10 px-20">
-        <nav className="flex justify-center lg:py-10 h-[90vh] basis-[15rem] flex-grow-0 overflow-auto">
+      <div className=" flex mx-auto gap-x-10">
+        <nav className="flex justify-center lg:py-10 h-[90vh] basis-[20%] flex-grow-0 overflow-auto pl-20 ">
           <div className="flex flex-col justify-between items-center w-[200px] px-2">
             <div className="space-y-5 text-[#F1ADB0] text-lg w-full">
               <h2 className="text-black font-bold text-xl px-3">GUIDE:</h2>
@@ -50,7 +65,7 @@ export const ScreeningPage = () => {
                     {item.name}
                   </h3>
                   {selectedSection === item.id && (
-                    <ul className="text-black font-bold px-3 mt-1">
+                    <ul className="text-black font-bold px-3 mt-1 whitespace-nowrap ">
                       {item.subSections.map((subsection, index) => (
                         <li
                           className={
@@ -63,55 +78,72 @@ export const ScreeningPage = () => {
                           }
                           key={index}
                         >
-                          {subsection.name}
+                          {subsection.name.length > 18 ? subsection.name.slice(0,18) + '...' : subsection.name}
                         </li>
                       ))}
                     </ul>
                   )}
                 </div>
               ))}
+              {getRecommendations && (
+                <h3
+                  className={
+                    "cursor-pointer px-3 py-1 rounded-[25px] bg-primary text-white font-bold"
+                  }
+                >
+                  Recommendations
+                </h3>
+              )}
             </div>
 
-						<div className="text-[#99B3DD] space-y-3 w-full flex flex-col items-center px-3 ">
-							<Button
-								onClick={startScreening}
-								sx={{
-									backgroundColor: '#DAE7F6',
-									color: '#9FB9E0',
-									border: '2px solid #92AEDB',
-									fontWeight: 'bold',
-									textTransform: 'none',
-									fontSize: '0.9rem',
-									minWidth: '100%',
-									marginTop: 5,
-								}}
-								disableElevation
-							>
-								Start Screening
-							</Button>
-							<Button
-								onClick={viewHealthRecords}
-								sx={{
-									color: '#92AEDB',
-									textTransform: 'none',
-									fontWeight: 'bold',
-									fontSize: '0.9rem',
-								}}
-							>
-								View Health Records {'>'}
-							</Button>
-						</div>
-					</div>
-				</nav>
+            <div className="text-[#99B3DD] space-y-3 w-full flex flex-col items-center px-3 ">
+              <Button
+                onClick={startScreening}
+                sx={{
+                  backgroundColor: "#DAE7F6",
+                  color: "#9FB9E0",
+                  border: "2px solid #92AEDB",
+                  fontWeight: "bold",
+                  textTransform: "none",
+                  fontSize: "0.9rem",
+                  minWidth: "100%",
+                  marginTop: 5,
+                }}
+                disableElevation
+              >
+                Start Screening
+              </Button>
+              <Button
+                onClick={viewHealthRecords}
+                sx={{
+                  color: "#92AEDB",
+                  textTransform: "none",
+                  fontWeight: "bold",
+                  fontSize: "0.9rem",
+                  width: '100%',
+                  paddingHorizontal: 3
+                }}
+              >
+                View Health Records {">"}
+              </Button>
+            </div>
+          </div>
+        </nav>
 
         <main className=" min-h-[90vh] flex-grow-2 basis-[80%] ">
-          <MultiStepForm
-            selectedSection={selectedSection}
-            selectedSubSection={selectedSubSection}
-            handleSelectedSection={handleSelectedSection}
-            formData={formData}
-            setFormData={setFormData}
-          />
+          {getRecommendations ? (
+            <GetRecommendations />
+          ) : (
+            <MultiStepForm
+              selectedSection={selectedSection}
+              selectedSubSection={selectedSubSection}
+              handleSelectedSection={handleSelectedSection}
+              formData={formData}
+              setFormData={setFormData}
+              setGetRecommendations={setGetRecommendations}
+              handleGetRecommendations={handleGetRecommendations}
+            />
+          )}
         </main>
       </div>
     </>
