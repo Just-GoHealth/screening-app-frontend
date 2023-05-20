7330ec9e058813be45a33252f5a964cfc8349f1f
boypaida12 <baddysackey5@gmail.com>
2023-05-20 10:51:40 +0000

    Merge pull request #11 from Just-GoHealth/main
    Updated landing page image, added add new school page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './views/home/HomePage';
 import { UserHealthSummaryPage } from './views/user-health-summary';
@@ -8,6 +10,8 @@ import { ScreeningPage } from './views/screening';
 import { SchoolHealthRecordsPage } from './views/school-health-records';
 import { SchoolHealthSummaryPage } from './views/school-health-summary';
 import { AccessHealthRecords } from './views/access-health-records/AccessHealthRecords';
+import { AddNewSchool } from './views/add-new-school/AddNewSchool';
+import { ProtectedRoute } from './shared/feature/authentication/ProtectedRoute';
 
 const App = () => {
 	return (
@@ -17,67 +21,42 @@ const App = () => {
 				<Route
 					path="/access-health-records"
 					element={<AccessHealthRecords />}
-				></Route>
-				<Route path="/all-health-records" element={<AllHealthRecordsPage />} />
-				<Route
-					path="/school-health-records"
-					element={<SchoolHealthRecordsPage />}
 				/>
 				<Route
-					path="/school-health-summary"
-					element={<SchoolHealthSummaryPage />}
+					path="/add-new-school"
+					element={<ProtectedRoute page={<AddNewSchool />} />}
 				/>
 				<Route
-					path="/user-health-summary"
-					element={<UserHealthSummaryPage />}
+					path="/all-health-records"
+					element={<ProtectedRoute page={<AllHealthRecordsPage />} />}
+				/>
+				<Route
+					path="/school-health-records/:schoolId"
+					element={<ProtectedRoute page={<SchoolHealthRecordsPage />} />}
+				/>
+				<Route
+					path="/school-health-summary/:schoolId"
+					element={<ProtectedRoute page={<SchoolHealthSummaryPage />} />}
+				/>
+				<Route
+					path="/user-health-summary/:userId"
+					element={<ProtectedRoute page={<UserHealthSummaryPage />} />}
 				/>
 
 				<Route path="/screening" element={<ScreeningPage />} />
 
 				<Route path="*" element={<PageNotFoundPage />} />
 			</Routes>
+
+			<ToastContainer
+				limit={5}
+				position={'bottom-left'}
+				autoClose={3000}
+				draggable={true}
+				pauseOnFocusLoss={false}
+			/>
 		</>
 	);
-
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./views/home/HomePage";
-import { SchoolHealthRecordsPage } from "./views/school-health-records/SchoolHealthRecordsPage";
-import { SchoolHealthSummaryPage } from "./views/school-health-summary/SchoolHealthSummaryPage";
-import { UserHealthSummaryPage } from "./views/user-health-summary";
-import { AllHealthRecordsPage } from "./views/all-health-records";
-import { PageNotFoundPage } from "./views/page-not-found";
-import { AccessHealthRecords } from "./views/access-health-records/AccessHealthRecords";
-import { AddNewSchool } from "./views/add-new-school/AddNewSchool";
-
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/access-health-records"
-          element={<AccessHealthRecords />}
-        />
-        <Route path="/add-new-school" element={<AddNewSchool />} />
-        <Route path="/all-health-records" element={<AllHealthRecordsPage />} />
-        <Route
-          path="/school-health-records"
-          element={<SchoolHealthRecordsPage />}
-        />
-        <Route
-          path="/school-health-summary"
-          element={<SchoolHealthSummaryPage />}
-        />
-        <Route
-          path="/user-health-summary"
-          element={<UserHealthSummaryPage />}
-        />
-
-        <Route path="*" element={<PageNotFoundPage />} />
-      </Routes>
-    </>
-  );
 };
 
 export default App;
