204790d25d309ef7b031eef2df80ddf5c0c6ab47
Philemon Forson <firstkoolaid21@gmail.com>
2023-05-23 11:35:55 +0000

    school health summary details rendered

--- src/shared/components/health-results/SchoolResults.jsx
+++ src/shared/components/health-results/SchoolResults.jsx
@@ -1,67 +1,72 @@
-import React from 'react';
-import { Recommendations } from './helpers/Recommendations';
-import { SectionHeading } from './helpers/SectionHeading';
-import { ScreeningList } from './helpers/ScreeningList';
-import { SchoolSignsSymptoms } from './helpers/SchoolSignsSymptoms';
-import { GroupSign } from './helpers/GroupSign';
-import './styles.css';
-import { Notice } from './helpers/Notice';
+import React from "react";
+import { Recommendations } from "./helpers/Recommendations";
+import { SectionHeading } from "./helpers/SectionHeading";
+import { ScreeningList } from "./helpers/ScreeningList";
+import { SchoolSubsection } from "./helpers/SchoolSubsection";
+import { GroupSign } from "./helpers/GroupSign";
+import "./styles.css";
+import { Notice } from "./helpers/Notice";
+import data from "../../data/data.json";
 
 export const SchoolResults = ({
-	title,
-	subTitle,
-	date,
-	screeningReport,
-	recommendations,
+  title,
+  subTitle,
+  date,
+  screeningReport,
+  recommendations,
 }) => {
-	return (
-		<>
-			<div className="health-summary-heading-group">
-				<h4 className="health-summary-main-heading">{title}</h4>
-				<h4 className="health-summary-sub-heading">{subTitle}</h4>
-				<h4 className="health-summary-sub-heading">{date}</h4>
-			</div>
+  return (
+    <>
+      <div className="health-summary-heading-group">
+        <h4 className="health-summary-main-heading">{title}</h4>
+        <h4 className="health-summary-sub-heading">{subTitle}</h4>
+        <h4 className="health-summary-sub-heading">{date}</h4>
+      </div>
 
-			<div className="space-y-10 max-w-6xl mx-auto text-lg">
-				<div>
-					<SectionHeading heading="Screening Report" showSeparator={false} />
+      <div className="space-y-10 max-w-[75rem] mx-auto text-lg">
+        <div>
+          <SectionHeading heading="Screening Report" showSeparator={false} />
 
-					<ul className="list-disc space-y-7">
-						<ScreeningList />
-					</ul>
-				</div>
+          <ul className="list-disc space-y-7">
+            <ScreeningList />
+          </ul>
+        </div>
 
-				<div>
-					<SectionHeading heading="Recommendations" showSeparator={false} />
+        <div>
+          <SectionHeading heading="Recommendations" showSeparator={false} />
 
-					<div className="">
-						<Recommendations subTitle={'- 20 (15%)'} />
-					</div>
-				</div>
+          <div className="">
+            <Recommendations subTitle={"- 20 (15%)"} />
+          </div>
+        </div>
 
-				<>
-					<Notice />
-				</>
+        <>
+          <Notice />
+        </>
 
-				<div className="mb-10">
-					<SectionHeading
-						heading="Signs & Symptoms"
-						subHeading={
-							<>
-								<GroupSign
-									sign="No Significant Emotional Disorder"
-									number={20}
-								/>{' '}
-								<GroupSign sign="Mild Symptoms" number={40} />
-							</>
-						}
-					/>
+        {data.data.slice(1).map((section) => (
+          <div className="mb-10">
+            <SectionHeading
+              heading={section.name}
+              subHeading={
+                <>
+                  <GroupSign
+                    sign="No Significant Emotional Disorder"
+                    number={20}
+                  />{" "}
+                  <GroupSign sign="Mild Symptoms" number={40} />
+                </>
+              }
+            />
 
-					<ol className="list-decimal space-y-7">
-						<SchoolSignsSymptoms />
-					</ol>
-				</div>
-			</div>
-		</>
-	);
+            <ol className="list-decimal space-y-7">
+              {section.subSections.map((subSection) => (
+                <SchoolSubsection sectionName={subSection.name} questions={subSection.questions} />
+              ))}
+            </ol>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
---
+++ src/shared/components/health-results/helpers/SchoolSubsection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { RxDotFilled } from "react-icons/rx";
+import { GroupSign } from "./GroupSign";
+import { GroupSignTag } from "./GroupSignTag";
+
+export const SchoolSubsection = ({ sectionName, questions }) => {
+  return (
+    <li className="text-[#965AA4]">
+      {sectionName}
+      <GroupSign sign="No Significant Emotional Disorder" number={20} />{" "}
+      <GroupSign sign="Mild Symptoms" number={40} />
+      <table className="table-fixed text-black mt-2">
+        <tbody className="overflow-none">
+          {questions.map((question) => (
+            <tr className="overflow-none">
+              <td className="w-[17rem]">
+                <RxDotFilled className="inline text-[#965AA4] mb-1" />
+                {question.title}
+              </td>
+              <td className="max-w-1/12 text-center px-2">-</td>
+              {question.options.map((option) => (
+                <td className="min-w-1/3 px-1">
+                    <GroupSignTag
+                      signResult={option?.name ?? option}
+                      number={"30"}
+                    />
+                </td>
+              ))}
+            </tr>
+          ))}
+
+          {/* <tr>
+            <td className="min-w-1/12">
+              <RxDotFilled className="inline text-[#965AA4] mb-1" /> Feeling
+              Inadequate or Worthless
+            </td>
+            <td className="w-1/12 text-center">-</td>
+            <td className="min-w-6/12 space-x-2">
+              <GroupSignTag signResult={"No, not at all"} number={"30"} />
+              <GroupSignTag signResult={"Sometimes"} number={"30"} />
+            </td>
+          </tr> */}
+        </tbody>
+      </table>
+    </li>
+  );
+};
