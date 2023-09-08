b2c93a575ebf9d04345a8f9674b509a496c67202
Eyram Quashie <eyramkobla.quashie@gmail.com>
2023-09-08 10:11:40 +0000

    fix: removed unsudes imports

--- src/shared/components/manage-account/ManageAccountContent.jsx
+++ src/shared/components/manage-account/ManageAccountContent.jsx
@@ -7,6 +7,7 @@ import Heading from "../heading/heading.jsx";
 import { useQuery } from "react-query";
 import { useSchoolContext } from "../../context/SchoolContext.jsx";
 import { useUserContext } from "../../context/UserContext.jsx";
+import { BsPersonFill } from "react-icons/bs";
 
 const ManageAccountContent = ({ setShowModal }) => {
   const { user, isAlpha, logout } = useAuthContext()
@@ -36,41 +37,6 @@ const ManageAccountContent = ({ setShowModal }) => {
     },
   ]
 
-  function showAccountDetails(title) {
-    return (
-      <div>
-        <div className="bg-black rounded-t-xl text-white p-4">
-          <h3 className="font-bold text-xl">{title}
-            {isAlpha() ? <span
-              className="ml-10 rounded-full border border-2 border-white px-2 py-1">{Array.isArray(users) ? users.length : 0}</span> : (<div></div>)}
-          </h3>
-        </div>
-        <div className="border border-[3px] border-black rounded-b-xl space-y-2 p-3">
-          {(isAlpha() && Array.isArray(users)) ? users.map((user, i) => (
-            <div key={i} className="flex space-x-4">
-              <h3>{user.full_name}</h3>
-              <span
-                className="border border-gray-600 border-2 rounded-full p-0.5 text-xs font-semibold text-gray-600">200</span>
-            </div>
-          )) : (
-            <div className="space-y-2">
-              <table>
-                {userDetails.map((detail, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{detail.name}</td>
-                      <td className="pl-5 text-gray-400">{user[detail?.value]}</td>
-                    </tr>
-                  )
-                })}
-              </table>
-            </div>
-          )}
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className="p-8 space-y-10">
       <div className="flex justify-between items-center">
@@ -85,10 +51,49 @@ const ManageAccountContent = ({ setShowModal }) => {
         <button className="auth-button border-0 bg-black text-white pt-2" onClick={() => logout()}>Log out</button>
       </div>
       <div className="flex justify-center space-x-10">
-        {isAlpha() ? showAccountDetails('Account') : showAccountDetails('Your Page')}
-        <div className="w-4/12">
+        {/* Account Details */}
+        <div>
+          <div className="bg-black rounded-t-xl text-white py-2 px-4">
+            <div className="w-full flex space-x-3 justify-between items-center">
+              {!isAlpha() ? (<BsPersonFill className="text-4xl"/>) : null}
+              <h3 className="flex text-2xl">{isAlpha() ? 'Accounts' : 'Your page'}</h3>
+              {isAlpha() ? (
+                <span
+                  className="ml-10 rounded-full border border-2 border-white px-2 py-1">{Array.isArray(users) ? users.length : 0}</span>
+              ) : (
+                <div>Toggle</div>)}
+            </div>
+          </div>
+          <div className="border border-[3px] border-black rounded-b-xl space-y-2 p-3">
+            {(isAlpha() && Array.isArray(users)) ? users.map((user, i) => (
+              <div key={i} className="flex space-x-4">
+                <h3>{user.full_name}</h3>
+                <span
+                  className="border border-gray-600 border-2 rounded-full p-0.5 text-xs font-semibold text-gray-600">200</span>
+              </div>
+            )) : (isAlpha() && Array.isArray(users) && user.length === 0) ? <p>No users found</p> : (
+              <div className="space-y-2">
+                <table>
+                  <tbody>
+                  {userDetails.map((detail, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{detail.name}</td>
+                        <td className="pl-5 text-gray-400">{user[detail?.value]}</td>
+                      </tr>
+                    )
+                  })}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
+        </div>
+        {/* Schools */}
+        <div className="min-w-[300px]">
           <div className="flex justify-between items-center mb-2">
-            <h2 className="font-black text-primary text-2xl">Programs.<span className="text-black">10</span></h2>
+            <h2 className="font-black text-primary text-2xl">Programs.<span
+              className="text-black">{schools?.school_list?.length}</span></h2>
             {isAlpha() ? <AiOutlinePlusCircle
               onClick={() => handleAddSchool()}
               className="font-bold text-primary text-3xl cursor-pointer"/> : null}
@@ -96,7 +101,7 @@ const ManageAccountContent = ({ setShowModal }) => {
           <hr className="border-b-[1px] border-gray-400 mb-4"/>
           {/* List of Schools*/}
           <div className="space-y-1">
-            {Array.isArray(schools?.school_list) && filterSchools(schools?.school_list).map((school, i) => (
+            {Array.isArray(schools?.school_list) ? filterSchools(schools?.school_list).map((school, i) => (
               <div key={i} className="flex items-center space-x-3">
                 <div
                   className="w-[270px] flex justify-between items-center border border-2 border-primary rounded-lg px-2">
@@ -107,7 +112,7 @@ const ManageAccountContent = ({ setShowModal }) => {
                   onClick={() => handleAddSchool()}
                   className="font-bold text-gray-400 text-lg cursor-pointer"/> : null}
               </div>
-            ))}
+            )) : <p>No schools found</p>}
           </div>
         </div>
       </div>
